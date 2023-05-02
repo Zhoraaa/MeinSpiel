fa1a7ecefedb82623b4ff768f861f858e4a15a35
ILDARON123 <123239094+ILDARON123@users.noreply.github.com>
2023-05-02 14:16:55 +0500

    ver 0.1.5a
    Добавлено изменение данных пользователя

--- account.php
+++ account.php
@@ -6,7 +6,7 @@ require_once('pageBase.php');
     <div id="avatar"><img src="../img/account.svg" alt=""> </div>
     <div id="info">
         <div><h2><?= $user['balance'] ?> ₽</h2><button class="radius inter fifaks">Пополнить баланс</button></div>
-        <div><div class="inner-shadow radius fifaks pad"><?= $user['name_user'] ?></div><button class="radius inter fifaks">Редактировать</button></div>
+        <div><div class="inner-shadow radius fifaks pad"><?= $user['name_user'] ?></div><a href="../edit.php"><button class="radius inter fifaks">Редактировать</button></a></div>
         <div><div class="inner-shadow radius fifaks pad"><?= $user['email'] ?></div><a href="../user/logOut.php"><button class="radius inter fifaks">Выйти</button></a></div>
     </div>
 </div>
